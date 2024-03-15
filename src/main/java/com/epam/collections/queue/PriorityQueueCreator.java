f0a4ed2a84dc7dc1e55a86cb9746aac6a5f956fc
Darkhan Zhursin <daratechn@gmail.com>
2024-03-15 09:02:02 -0500

    done

--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,19 @@
 package com.epam.collections.queue;
 
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        PriorityQueue<String> priorityQueue =
+                new PriorityQueue<>((o1, o2) -> -o1.compareTo(o2));
+
+        for (String s : firstList) {
+            priorityQueue.add(s);
+        }
+        for (String s : secondList) {
+            priorityQueue.add(s);
+        }
+
+        return priorityQueue;
     }
 }
